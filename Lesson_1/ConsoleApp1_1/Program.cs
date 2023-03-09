c5220bef813f1e06d25e1b407cb2adfe645ff78b
cash2000@bk.ru <cash2000@bk.ru>
2023-03-09 10:50:26 +0300

    Сделали задачу 1_1

---
+++ Lesson_1/ConsoleApp1_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите прогорамму, которая на вход принимает два числа
+// и проверяет является ли первое число квадратом .
+int num1 = 0;
+int num2 = 0;
+
+Console.WriteLine("Введите пожалуйста число 1 n1 = ");
+num1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите пожалуйста число 2 n2 = ");
+num2 = int.Parse(Console.ReadLine()!);
+
+if ((num2 * num2) == num1)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
