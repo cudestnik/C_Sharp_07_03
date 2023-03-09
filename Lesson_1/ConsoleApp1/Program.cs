c62cf4908767662b8e14ac721737852301eba82a
cash2000@bk.ru <cash2000@bk.ru>
2023-03-09 09:40:53 +0300

    Сщздали консольное приложение и решили Задачу 0

---
+++ Lesson_1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число выдаёт его
+// квадрат(число умноженное само на себя)
+
+
+Console.WriteLine("Write a number: ");
+
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(num * num);
+
+
+
