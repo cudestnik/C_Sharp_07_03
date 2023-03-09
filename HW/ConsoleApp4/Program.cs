9037a14e89d994d36b11ff0ed07cae6cc4d0db41
cash2000@bk.ru <cash2000@bk.ru>
2023-03-09 16:18:34 +0300

    Сделал задачу 4

---
+++ HW/ConsoleApp4/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 4. Напишите программу, которая на вход принимает
+//число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int num = 0;
+
+Console.WriteLine("Введите пожалуйста число:");
+num = int.Parse(Console.ReadLine()!);
+
+for(int i = 2; i < num; i+=2)
+
+    Console.WriteLine(i);
+
