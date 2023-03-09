823396ed0323e9a14a09935718df560352de14d1
cash2000@bk.ru <cash2000@bk.ru>
2023-03-09 14:48:23 +0300

    Решил домашнее задание 1.

---
+++ HW/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1. Напишите программу, на вход принимает два числа 
+// и выдает, какое число большее,а какое меньшее.
+
+
+Console.WriteLine("Введите пожалуйста число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите пожалуйста число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2)
+{
+    Console.WriteLine("Максимальное число: " + num1);
+}
+else if (num1 < num2)
+{
+    Console.WriteLine("Максимальное число: " + num2);
+}
+else if (num1 == num2)
+{
+    Console.WriteLine("Числа равны! ");
+}
