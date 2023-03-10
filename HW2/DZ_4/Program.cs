815623afa8b3544b51abcd94212bd4457b3732bb
cash2000@bk.ru <cash2000@bk.ru>
2023-03-10 22:14:09 +0300

    Решил DZ_4

---
+++ HW2/DZ_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу,которая принимает на вход цифру,
+//обозначающую день недели, и проверяет является ли этот день выходным.
+Console.WriteLine("Введите пожалуйста число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+
+if (num >=6 && num <=7)
+{
+    Console.WriteLine("Да ");
+}
+else if (num >= 1 && num <=5)
+{
+    Console.WriteLine("Нет ");
+}
+
+
