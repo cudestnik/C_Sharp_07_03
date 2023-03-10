a9b490349bf58579a21328c552ac94be1b6a13db
cash2000@bk.ru <cash2000@bk.ru>
2023-03-10 20:57:32 +0300

    Решил DZ_1

---
+++ HW2/DZ_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+
+int num = int.Parse(Console.ReadLine()!);
+
+int result = 0;
+
+result = ((num % 100) / 10);
+
+    Console.WriteLine(result);
+
+
