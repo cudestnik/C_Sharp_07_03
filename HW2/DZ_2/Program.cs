f1f513d4c5b1872b9867b26daf4683c9d7d89967
cash2000@bk.ru <cash2000@bk.ru>
2023-03-10 21:11:48 +0300

    Решил задачу 2 - DZ_2

---
+++ HW2/DZ_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит случайное трехзначное число 
+//и удалаяет вторую цифру этого числа.
+
+int num = new Random().Next(100, 1000);
+
+Console.WriteLine(num);
+int result = 0;
+
+result = ((num /100 )*10) + (num %10);
+
+Console.WriteLine(result);
+
