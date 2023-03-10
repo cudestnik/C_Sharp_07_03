fc1e601275b1894f4242e6639d7977d51ab4bc2b
cash2000@bk.ru <cash2000@bk.ru>
2023-03-10 20:32:01 +0300

    Решили задачу 4

---
+++ Lesson_2/ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет 
+// кратно ли оно одновременно 7 и 23?
+
+
+
+
+int num = int.Parse(Console.ReadLine()!);
+
+void MyFunc(int num)
+{
+
+    if (num % 7 == 0 && num % 23 == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        Console.WriteLine("Не Кратно ");
+    }
+
+}
+
+MyFunc(num);
