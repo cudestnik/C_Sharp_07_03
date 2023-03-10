e69400705d2a1e2821ada50519c188c325697774
cash2000@bk.ru <cash2000@bk.ru>
2023-03-10 21:33:59 +0300

    Решил DZ_3

---
+++ HW2/DZ_3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая выводит третью цифру данного
+// числа или сообщает, чтто третьей цифры нет.
+
+int num = new Random().Next(10, 1000);
+
+Console.WriteLine(num);
+int result = 0;
+if (num < 999 && num > 99)
+{
+    result = num % 10;
+    Console.WriteLine(result);
+}
+else if (num < 100)
+{
+    Console.WriteLine("Третьей цифры нет ");
+}
