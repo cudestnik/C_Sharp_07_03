096d1fb2bf2333664e6709b7327735b68beca00b
cudestnik <cash2000@bk.ru>
2023-03-15 13:20:44 +0300

    Решил задачу 1 HW-3

---
+++ HW3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает
+//пятизначное число  и проверяет, является ли оно палиндромом.
+int num = int.Parse(Console.ReadLine()!);
+
+int result_1 = 0;
+
+int result_2 = 0;
+
+int result_3 = 0;
+
+int result_4 = 0;
+
+
+result_1 = (num / 10000);
+result_2 = ((num / 1000) % 10);
+result_3 = ((num % 100)/10);
+result_4 = ((num % 10));
+
+if(result_1 == result_4 && result_2 == result_3)
+{
+    Console.WriteLine("Да ");
+}
+else
+{
+    Console.WriteLine("Нет ");
+}
