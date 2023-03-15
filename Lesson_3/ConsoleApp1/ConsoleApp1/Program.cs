4bf68f80abb90ea77368306a08a8bd69b935d88c
cudestnik <cash2000@bk.ru>
2023-03-15 12:20:28 +0300

    Решили в классе 3 задачу

--- Lesson_3/ConsoleApp1/ConsoleApp1/Program.cs
+++ Lesson_3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая по заданному номеру четверти
+// показывает диапазон возможных координат точек в этой четверти(х и у).
+GetQuarter();
+
+void GetQuarter ()
+{
+    Console.Write("Please enter quarter = ");
+    int q = int.Parse(Console.ReadLine()!);
+
+    if(q < 1 || q > 4)
+    {
+        Console.WriteLine("No such quarter ");
+    }
+    else
+    {
+        switch (q)
+        {
+            case 1:
+                Console.WriteLine("x > 0, y > 0 ");
+                break;
+                case 2:
+                Console.WriteLine("x < 0, y > 0 ");
+                break;
+                case 3:
+                Console.WriteLine("x < 0, y < 0 ");
+                break;
+                case 4:
+                Console.WriteLine("x > 0, y < 0 ");
+                break;
+                default:
+                Console.WriteLine("No such quarter ");
+                break;
+        }
+    }
+}
+
---
+++ Lesson_3/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая принимает на вход координаты двух
+//точек и находит расстояние между ними в 2 D
+//пространстве.
+
+Console.Write("x1 = ");
+int x1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("y1 = ");
+int y1 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("x2 = ");
+int x2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("y1 = ");
+int y2 = int.Parse(Console.ReadLine()!);
+
+double Lenght (int x1,int y1, int x2,int y2)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2)),3);
+}
+Console.Write(Lenght(x1, y1, x2, y2));
