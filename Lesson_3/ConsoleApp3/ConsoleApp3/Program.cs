7f4faaed889956cc58fa742ec208e6c4a2a4b680
cudestnik <cash2000@bk.ru>
2023-03-15 12:42:21 +0300

    Решили в классе 4 задачу

---
+++ Lesson_3/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программуБ которая принимает на вход число (N)
+// и выдаёт таблицу квадратов чисел от 1 до N
+
+
+void Pow(int num)
+{
+    int i = 1;
+
+    while (i <= num)
+    {
+           Console.WriteLine(Math.Pow(i,2));
+        i++;        
+    }
+}
+int number = int.Parse(Console.ReadLine()!);
+Pow(number);
