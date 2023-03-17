6c26b3c13293839e6fcaccdad0fd5b099403abc8
cudestnik <cash2000@bk.ru>
2023-03-17 15:41:27 +0300

    Решили 3 задачу в классе

---
+++ Lesson_4/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход число 
+// N и выдаёт произведение числе от 1 до N.
+
+int num = GetUserNumber();
+int digits = MultiplyNumbers(num);
+Console.WriteLine(digits);
+
+int GetUserNumber()
+{
+
+    int number = 0;
+    while (number == 0)
+    {
+        Console.Write("Пожалуйста введите число: ");
+        number = int.Parse(Console.ReadLine()!);
+    }
+    return number;
+}
+
+int MultiplyNumbers(int n)
+{
+
+    int multi = 1;   
+    for(int i = 1; i <= n; i++)
+    {
+        multi = multi * i;
+    }
+    return multi;
+}
