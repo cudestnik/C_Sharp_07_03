6ccbf6fb2c1bf3ea8b4772f6e3df5a4be32c1408
cudestnik <cash2000@bk.ru>
2023-03-17 17:30:59 +0300

    Решил задачау 1 Домашнего задания.

--- HW4/ConsoleApp1/ConsoleApp1/Program.cs
+++ HW4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,23 @@
 ﻿// Задача 1. Напишите цикл ,который принимает на вход два числа (А и В )
 // и возводит число А в натуральную степень В.
+int num1 = 0;
+int num2 = 0;
+
+Console.Write("Введите пожалуйста число A = ");
+num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите пожалуйста число B = ");
+num2 = int.Parse(Console.ReadLine()!);
+int result = 1;
 
 
+for (int i = 1; i <= num2; i++)
+{
+
+   result = result * num1;
+    
+      
+}   
+    Console.WriteLine(result);
+
+Console.ReadKey();
