3705220a2646e27c963035a2d979939d91da9d9d
cudestnik <cash2000@bk.ru>
2023-04-03 10:52:18 +0300

    решили задачу 2 в классе

--- Lesson_8/ConsoleApp2/ConsoleApp2/Program.cs
+++ Lesson_8/ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,5 +1,7 @@
-﻿// 1. Задайте двумерный массив. Напишите программу,
-//    которая поменяет местами первую и последнюю строку массива.
+﻿// 2. Задайте двумерный массив. Напишите программу,
+//    которая заменяет строки на столбцы. В случае, если это невозможно,
+//    программа должна вывести сообщение для пользователя.
+
 
 void Print(int[,] arr)
 {
@@ -24,13 +26,22 @@ int[,] MassNums(int row, int column, int from, int to)
             arr[i, j] = new Random().Next(from, to);
     return arr;
 }
-void FirstLast(int[,] arr)
-{
-    int row_size = arr.GetLength(0);
-    int column_size = arr.GetLength(1);
 
-    for (int j = 0; j < column_size; j++)
-        (arr[0, j], arr[row_size - 1, j]) = (arr[row_size - 1, j], arr[0, j]);
+string MatrixT(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    if (row != column) return "Matrix transposition is not possible";
+
+    for (int i = 1; i < row; i++)
+    {
+        for (int j = 0; j < i; j++)
+           // j = i
+            (arr[i, j], arr[j, i]) = (arr[j, i], arr[i, j]);
+    }
+
+    return "Matrix transposition is possible"; ;
 }
 
 
@@ -47,5 +58,7 @@ int stop = int.Parse(Console.ReadLine()!);
 int[,] mass = MassNums(row_num, column_num, start, stop);
 
 Print(mass);
-FirstLast(mass);
+
+string answer = MatrixT(mass);
+Console.WriteLine(answer);
 Print(mass);
