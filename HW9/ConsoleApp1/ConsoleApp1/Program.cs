28ded62b4e161b910f7455ebbc2e4203193ef267
cudestnik <cash2000@bk.ru>
2023-04-05 12:58:19 +0300

    Решил задачау 1 домашнее задание.

--- HW9/ConsoleApp1/ConsoleApp1/Program.cs
+++ HW9/ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,9 +1,8 @@
 ﻿/*
- Задача 1 Задайте значения N. Напишите программу, которая выведет все чётные
+Задача 1: Задайте значения N. Напишите программу, которая выведет все чётные 
 натуральные числа в промежутке от N до 1.
 
- */
-
+*/
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 1;
@@ -12,22 +11,14 @@ NaturalToLow(number, count);
 
 void NaturalToLow(int n, int count)
 {
+    if (count % 2 != 0)
+    {
+        count++;
+    }
     if (count > n)
     {
         return;
     }
-
-    if (count > n)
-        return;
-    if (n % 2 == 0)
-    {
-        return;
-    }
-   
-    else
-    {
-        NaturalToLow(n, count + 1);
-        Console.Write(count + " ");
-    }
+    NaturalToLow(n, count + 2);
+    Console.Write(count + " ");
 }
-
