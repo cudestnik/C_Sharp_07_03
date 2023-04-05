593131f836706f55a7bf8c86c08e08a6f4bf4a38
cudestnik <cash2000@bk.ru>
2023-04-05 09:32:48 +0300

    Решил задачу 2.

---
+++ HW9/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ Задача 1 Задайте значения N. Напишите программу, которая выведет все чётные
+натуральные числа в промежутке от N до 1.
+
+ */
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+NaturalToLow(number, count);
+
+
+void NaturalToLow(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+
+    if (count > n)
+        return;
+    if (n % 2 == 0)
+    {
+        return;
+    }
+   
+    else
+    {
+        NaturalToLow(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
+
